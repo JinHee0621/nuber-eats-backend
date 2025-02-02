ece7fd9236188f53842d41f0622d39aa9dbfc967
jinheehan <lsd0017@naver.com>
2025-02-02 13:43:07 +0900

    250202 User Login Add

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "./entities/user.entity";
 import { Repository } from "typeorm";
 import { Injectable } from "@nestjs/common";
+import { LoginInput } from './dtos/login.dto';
 
 @Injectable()
 export class UsersService{
@@ -10,18 +11,46 @@ export class UsersService{
         @InjectRepository(User) private readonly users: Repository<User>
     ) {}
 
-    async createAccount({email, password, role}:CreateAccountInput) : Promise<string | undefined> {
+    async createAccount({email, password, role}:CreateAccountInput) : Promise<{ok : boolean; error?: string}> {
         try{
             const exists = await this.users.findOne({where: {email}})
             if(exists) {
-                return "There is a user with that email already";
+                return { ok: false, error: "There is a user with that email already"};
             }
             await this.users.save(this.users.create({email, password, role}));
+            return {ok: true};
         }catch(e) {
-            return "Couldn't create account";
+            return {ok: false, error: "Couldn't create account"};
         }
         // check new user
         // create user & hash the password
 
+    }
+
+    async login({email, password} : LoginInput) : Promise<{ok : boolean; error?: string; token?: string}> {
+        try{
+            const user = await this.users.findOne({ where: {email} })
+            if(!user) {
+                return {
+                    ok:false,
+                    error: "User not found",
+                }
+            }
+            const passwordCurrect = await user.checkPassword(password);
+            if(!passwordCurrect) {
+                return {
+                    ok: false,
+                    error: "Wrong password"
+                }
+            }
+            return {
+                ok: true, token : 'testtoekn'
+            };
+        }catch(error) {
+            return {
+                ok: false,
+                error
+            }
+        }
     }
 }
